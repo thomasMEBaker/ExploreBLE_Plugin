e324a9f7c1be916e6c159455a25b13b32b3d9220
Tom Baker <thomasmebaker@gmail.com>
2024-09-10 10:55:51 +0100

    adding blank plugin

---
+++ Plugins/ExploreBLE/Source/ExploreBLE/Private/ExploreBLEBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ExploreBLEBPLibrary.h"
+#include "ExploreBLE.h"
+
+UExploreBLEBPLibrary::UExploreBLEBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UExploreBLEBPLibrary::ExploreBLESampleFunction(float Param)
+{
+	return -1;
+}
+
